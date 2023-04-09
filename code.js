133dff955a231717fe7385fb88a0d65bec69b328
mohit <mohitkataria631@gmail.com>
2023-04-09 19:27:52 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,18 @@
+let text = document.getElementById('text');
+let treeleft = document.getElementById('tree-left');
+let treeright = document.getElementById('tree-right');
+let gateleft = document.getElementById('gate-left');
+let gateright = document.getElementById('gate-right');
+
+
+window.addEventListener('scroll',() =>
+{
+    let value =window.scrollY;
+
+
+    text.style.marginTop = value * 2.5 + 'px';
+    treeleft.style.left = value * -1.5 + 'px';
+    treeright.style.left = value * 1.5 + 'px';
+    gateright.style.left = value * -0.5 + 'px';
+    gateleft.style.left = value * 0.5 + 'px';
+})
